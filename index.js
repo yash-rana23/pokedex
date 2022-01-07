11f9d360c3853f1a9fb1ec9bdb8e9b0ab1964bdf
YASH RANA <76786367+yash-rana23@users.noreply.github.com>
2022-01-07 18:03:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import Pokegame from './pokegame.js'
-import './inex.css'
+
 class App extends React.Component {
   render() {
     return (
